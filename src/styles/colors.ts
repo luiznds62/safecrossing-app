1827391b9c2d0b65d6055d1b00830b8c30835f78
Luiz Henrique <luiznds62@gmail.com>
2021-03-30 20:17:26 -0300

    initial folder config

---
+++ src/styles/colors.ts
@@ -0,0 +1,14 @@
+export const COLOR_PRIMARY = '#1779ba';
+export const COLOR_SECONDARY = '#767676';
+export const COLOR_WHITE = '#FFFFFF';
+export const COLOR_BLACK = '#000000';
+
+// ACTIONS
+export const COLOR_SUCCESS = '#3adb76';
+export const COLOR_WARNING = '#ffae00';
+export const COLOR_ALERT = '#cc4b37';
+
+// GRAYSCALE
+export const COLOR_GRAY_LIGHT = '#e6e6e6';
+export const COLOR_GRAY_MEDIUM = '#cacaca';
+export const COLOR_GRAY_DARK = '#8a8a8a';
